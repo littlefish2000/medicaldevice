caf35b1e977536395fe428d2f0e77c61a643621b
littlefish2000 <yuchenyang@gmail.com>
2016-07-08 09:38:09 -0400

    update CertificateCompany finder

--- src/main/java/ca/gc/hc/mds/domain/CertificateCompany.java
+++ src/main/java/ca/gc/hc/mds/domain/CertificateCompany.java
@@ -1,9 +1,12 @@
 package ca.gc.hc.mds.domain;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.EntityManager;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -13,7 +16,8 @@ import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
-@RooJpaActiveRecord(versionField = "",  schema = "MDSDB",identifierType = CertificateCompanyPK.class, table = "CERTIFICATE_COMPANY")
+@RooJpaActiveRecord(versionField = "",  schema = "MDSDB",identifierType = CertificateCompanyPK.class,
+table = "CERTIFICATE_COMPANY",finders = { "findCertificateCompanysByEntryDateEquals" })
 public class CertificateCompany {
 	
     /**
@@ -40,5 +44,13 @@ public class CertificateCompany {
     private Date entryDate; 
     
 	@Column(name="NAME_RATIONAL", columnDefinition = "char", nullable=false, length=280)	
-	private String nameRational;    
+	private String nameRational;  
+	
+    public static TypedQuery<CertificateCompany> findCertificateCompanysByCompanyAuthIdEquals(BigDecimal companyAuthId) {
+        if (companyAuthId == null) throw new IllegalArgumentException("The entryDate argument is required");
+        EntityManager em = CertificateCompany.entityManager();
+        TypedQuery<CertificateCompany> q = em.createQuery("SELECT o FROM CertificateCompany AS o WHERE o.id.companyAuthId = :companyAuthId", CertificateCompany.class);
+        q.setParameter("companyAuthId", companyAuthId);
+        return q;
+    }	
 }
