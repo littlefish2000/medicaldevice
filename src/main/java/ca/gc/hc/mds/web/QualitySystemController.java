fa9414fa76d3f851e4680559f3bc445debb5b0e4
littlefish2000 <yuchenyang@gmail.com>
2016-07-08 10:43:17 -0400

    add find certificate by companyid

--- src/main/java/ca/gc/hc/mds/web/QualitySystemController.java
+++ src/main/java/ca/gc/hc/mds/web/QualitySystemController.java
@@ -171,6 +171,26 @@ public class QualitySystemController {
     }
 	
 	
+	@RequestMapping(value = "/qualitysystem/certificatecompanys", params = { "find=ByCompanyIdLike", "form" }, method = RequestMethod.GET)
+    public String findCertificateCompanysByCompanyIIdLikeForm(Model uiModel) {
+        return "qualitysystem/certificatecompanys/findCertificateCompanysByByCompanyIdLike";
+    }
+
+	@RequestMapping(value = "/qualitysystem/certificatecompanys", params = "find=ByCompanyIdLike", method = RequestMethod.GET)
+    public String findCertificateCompanysByCompanyIIdLike(@RequestParam("certificateId") String certificateId, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("certificates", Certificate.findCertificatesByCertificateIdLike(certificateId, sortFieldName, sortOrder).setFirstResult(firstResult).setMaxResults(sizeNo).getResultList());
+            float nrOfPages = (float) Certificate.countFindCertificatesByCertificateIdLike(certificateId) / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("certificates", Certificate.findCertificatesByCertificateIdLike(certificateId, sortFieldName, sortOrder).getResultList());
+        }
+        addDateTimeFormatPatterns(uiModel);
+        return "qualitysystem/certificates/list";
+    }	
+	
 	@RequestMapping(value = "/qualitysystem/validity", params = { "batchtracking", "form" }, method = RequestMethod.GET)
     public String findBatchControl(Model uiModel) {
         return "qualitysystem/validity/batchtracking";
