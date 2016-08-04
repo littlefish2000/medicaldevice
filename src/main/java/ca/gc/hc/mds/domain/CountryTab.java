a0892963bbddd19139ee53066acd28cdb8581f47
littlefish2000 <yuchenyang@gmail.com>
2016-08-04 06:38:59 -0700

    add backend validate for countrytab

--- src/main/java/ca/gc/hc/mds/domain/CountryTab.java
+++ src/main/java/ca/gc/hc/mds/domain/CountryTab.java
@@ -4,6 +4,7 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -18,6 +19,7 @@ public class CountryTab {
     @Id
     @Type(type = "fixedLengthCharType")
     @Column(name = "COUNTRY_CD", columnDefinition = "char",nullable=false, length=2)
+    @Length(min = 2,max=2, message = "The field must be 2 characters")
     private  String countryCd;
     
     public String getCountryCdCd() {
@@ -26,6 +28,7 @@ public class CountryTab {
     
     @Column(name = "COUNTRY_DESC", columnDefinition = "char",nullable=false, length=25)
     @Type(type = "fixedLengthCharType")
+    @Length(max=25, message = "The field must less than 25 characters")
     private String countryDesc;
     public String getcountryDesc() {
         return countryDesc;
