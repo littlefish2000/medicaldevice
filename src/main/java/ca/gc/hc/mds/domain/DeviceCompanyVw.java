e44909e6bf3f9ca0e363d11c5c6898dd0e2f63f7
littlefish2000 <yuchenyang@gmail.com>
2016-08-10 11:53:49 -0700

    Add deviceCompanyview jpa entity

---
+++ src/main/java/ca/gc/hc/mds/domain/DeviceCompanyVw.java
@@ -0,0 +1,10 @@
+package ca.gc.hc.mds.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(table = "DEVICE_COMPANY_VW")
+public class DeviceCompanyVw {
+}
