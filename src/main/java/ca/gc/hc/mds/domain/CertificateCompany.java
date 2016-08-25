4017487dfb84ea1cc3c2e9ef0063c57d64e6d2f5
Hasan Issa <Hasan.issa@canada.ca>
2016-08-25 12:32:50 -0400

    QualitySystem>Certificates>SearchCertificate + Search Orig

--- src/main/java/ca/gc/hc/mds/domain/CertificateCompany.java
+++ src/main/java/ca/gc/hc/mds/domain/CertificateCompany.java
@@ -17,7 +17,7 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord(versionField = "",  schema = "MDSDB",identifierType = CertificateCompanyPK.class,
-table = "CERTIFICATE_COMPANY",finders = { "findCertificateCompanysByEntryDateEquals" })
+table = "CERTIFICATE_COMPANY",finders = { "findCertificateCompanysByCompanyIdLike" })
 public class CertificateCompany {
 	
     /**
@@ -54,3 +54,79 @@ public class CertificateCompany {
         return q;
     }	
 }
+
+package ca.gc.hc.mds.domain;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EntityManager;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "",  schema = "MDSDB",identifierType = CertificateCompanyPK.class,
+table = "CERTIFICATE_COMPANY",finders = { "findCertificateCompanysByCompanyIdLike" })
+@TypeDef(name = "fixedLengthCharType", typeClass = ca.gc.hc.mds.reference.OracleLengthCharType.class)
+
+public class CertificateCompany {
+	
+	
+
+	@Type(type = "fixedLengthCharType")
+	@Column(name = "NAME_RATIONAL", columnDefinition = "char", nullable = false, length = 280)
+	private String nameRational;
+
+	
+    /**
+     */
+    @NotNull
+    @Column(name = "STATUS_DT", columnDefinition = "DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date statusDate;
+    
+    /**
+     */
+    @NotNull
+    @Column(name = "RECEIPT_DT", columnDefinition = "DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date receiptDate;   
+    
+    /**
+     */
+    @Column(name = "ENTRY_DT", columnDefinition = "DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date entryDate; 
+    
+	
+    public static TypedQuery<CertificateCompany> findCertificateCompanysByCompanyAuthIdEquals(BigDecimal companyAuthId) {
+        if (companyAuthId == null) throw new IllegalArgumentException("The entryDate argument is required");
+        EntityManager em = CertificateCompany.entityManager();
+        TypedQuery<CertificateCompany> q = em.createQuery("SELECT o FROM CertificateCompany AS o WHERE o.id.companyAuthId = :companyAuthId", CertificateCompany.class);
+        q.setParameter("companyAuthId", companyAuthId);
+        return q;
+    }	
+    public static TypedQuery<CertificateCompany> findCertificateCompanysByCompanyIdEquals(BigDecimal companyId) {
+        if (companyId == null) throw new IllegalArgumentException("The entryDate argument is required");
+        EntityManager em = CertificateCompany.entityManager();
+        TypedQuery<CertificateCompany> q = em.createQuery("SELECT o FROM CertificateCompany AS o WHERE o.id.companyId = :companyId", CertificateCompany.class);
+        q.setParameter("companyId", companyId);
+        return q;
+    }
+
+		
+}
+
